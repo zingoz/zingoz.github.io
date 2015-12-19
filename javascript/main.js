bc3dc998df5448732fd2b824ec3568b893ad1baa
zingoz <ludvigroxin@hotmail.com>
2015-12-20 00:57:23 +0100

    Infographic
    Glömde ladda upp tidigare, har gjort basic utseende och börjat med
    javascript funktioner

--- javascript/main.js
+++ javascript/main.js
@@ -8,3 +8,28 @@ $(document).ready(function(){
     $(".b2div").hide();
   });
 });
+
+function stage(type){
+    var image = document.getElementById('pokeimage');
+  if (document.getElementById("radiofirst").checked==true) {
+    image.src = "img/" + type + "one.png";
+  }
+  else if (document.getElementById("radiosecond").checked==true) {
+    image.src = "img/" + type + "two.png";
+  }
+  else if (document.getElementById("radiothird").checked==true) {
+    image.src = "img/" + type + "three.png";
+  }
+}
+
+function changeimg() {
+  if (document.getElementById("radiograss").checked==true) {
+    stage("grass");
+  }
+  else if (document.getElementById("radiowater").checked==true) {
+    stage("water");
+  }
+  else if (document.getElementById("radiofire").checked==true) {
+    stage("fire");
+  }
+}
