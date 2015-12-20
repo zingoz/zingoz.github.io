a317103cb7b9cd63d67bb8cec2bb8e8a7a33581c
zingoz <ludvigroxin@hotmail.com>
2015-12-20 02:23:11 +0100

    problems and more problems
    satt timmar med ett problem som var något med chromes local server..

--- javascript/main.js
+++ javascript/main.js
@@ -9,16 +9,20 @@ $(document).ready(function(){
   });
 });
 
+
 function stage(type){
     var image = document.getElementById('pokeimage');
   if (document.getElementById("radiofirst").checked==true) {
     image.src = "img/" + type + "_one.png";
+    $("#pokeinfo").load("txt/" + type +"_one.txt");
   }
   else if (document.getElementById("radiosecond").checked==true) {
     image.src = "img/" + type + "_two.png";
+    $("#pokeinfo").load("txt/" + type +"_two.txt");
   }
   else if (document.getElementById("radiothird").checked==true) {
     image.src = "img/" + type + "_three.png";
+    $("#pokeinfo").load("txt/" + type +"_three.txt");
   }
 }
 
